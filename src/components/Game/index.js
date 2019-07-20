9928ccec26cddc36dda60880489d965501055a72
GordeySpb <gordey_89@mail.ru>
2019-07-20 20:20:49 +0300

    fix game logic

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Cell from '../Cell';
 
+import every from 'lodash/every';
+
 import { layout } from '../../helpers/index';
 
 class Game extends React.Component {
@@ -9,16 +11,25 @@ class Game extends React.Component {
     super(props);
   }
 
-  handleClick = value => {
-    const { moveCell, items } = this.props;
+  handleClick = index => {
+    const { items, moveCell } = this.props;
+    const cells = [...items];
     const emptyIndex = items.indexOf(0);
-    const targetIndex = items.indexOf(value);
+    const targetIndex = items.indexOf(index);
     const dif = Math.abs(targetIndex - emptyIndex);
 
-    if (dif == 1 || dif == 4) {
-      items[emptyIndex] = value;
-      items[targetIndex] = 0;
-      moveCell(items);
+    if (dif === 1 || dif === 4) {
+      cells[emptyIndex] = index;
+      cells[targetIndex] = 0;
+      moveCell({ cells });
+
+      let win = every(cells, (value, index, array) => {
+        value = value || 16;
+        return index === 0 || parseInt(array[index - 1]) <= parseInt(value);
+      });
+      if (win) {
+        console.log('win');
+      }
     }
   };
 
@@ -29,7 +40,7 @@ class Game extends React.Component {
       <div className="game">
         {items.map((i, key) => {
           const cellClass = key ? 'cell' : 'empty cell';
-          const [x, y] = layout[i];
+          const [x, y] = layout[items.indexOf(key)];
           const translate3d = `translate3d(${x}px,${y}px,0) scale(1.1)`;
 
           return (
--- src/reducers/board.js
+++ src/reducers/board.js
@@ -3,13 +3,14 @@ import { MOVE_CELL } from '../actions/index';
 import { getRandomPosition } from '../helpers/index';
 
 const initialState = {
-  items: getRandomPosition(),
+  // items: getRandomPosition(),
+  items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0],
 };
 
 const board = (state = initialState, { type, payload }) => {
   switch (type) {
     case MOVE_CELL:
-      return { items: payload };
+      return { items: [...payload.cells] };
 
     default:
       return state;
